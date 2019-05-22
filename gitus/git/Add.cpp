ba3f4ca11f3464ac9b5665fb29f643e9b3201b38
Jean-Christophe Normandeau <jcnorm1998@gmail.com>
2019-05-21 21:56:26 -0400

    Almost done with the add

--- gitus/git/Add.cpp
+++ gitus/git/Add.cpp
@@ -2,6 +2,7 @@
 #include <boost/uuid/detail/sha1.hpp>
 #include <boost/filesystem.hpp>
 #include <iostream>
+#include <vector>
 
 void add(const std::string& pathspec){
 	boost::filesystem::ifstream fileToHash(boost::filesystem::current_path() / pathspec);
@@ -14,14 +15,36 @@ void add(const std::string& pathspec){
 	}
 	fileToHash.close();
 
+
 	boost::uuids::detail::sha1 sha1;
 	sha1.process_bytes(contentToHash.data(), contentToHash.size());
 	unsigned hash[5] = { 0 };
 	sha1.get_digest(hash);
 
+	boost::filesystem::fstream indexFile(boost::filesystem::current_path() / ".git" / "index");
+	std::vector<std::string>* buffer = new std::vector<std::string>();
+	size_t indexToModify = -1;
 
-	boost::filesystem::ofstream indexFile;
-	indexFile.open(boost::filesystem::current_path() / ".git" / "index", boost::filesystem::ofstream::app);
-	indexFile << pathspec + "\t" << std::hex << hash[0] << hash[1] << hash[2] << hash[3] << hash[4] << std::endl;
+	while (!indexFile.eof()) { 
+		getline(indexFile, line);
+		buffer->push_back(line);
+		if (line.find(pathspec) != std::string::npos) {
+			indexToModify = buffer->size() - 1;
+		}
+	}
 	indexFile.close();
+
+	boost::filesystem::ofstream ofs(boost::filesystem::current_path() / ".git" / "index");
+	for (int i = 0; i < buffer->size(); i++) {
+		if (i == indexToModify) {
+			ofs << std::hex << hash[0] << hash[1] << hash[2] << hash[3] << hash[4] << " " + pathspec << std::endl;
+		} else {
+			ofs << std::string(buffer->at(i)) << std::endl;
+		}
+	}
+
+	if (indexToModify == -1) {
+		ofs << std::hex << hash[0] << hash[1] << hash[2] << hash[3] << hash[4] << " " + pathspec << std::endl;
+	}
+	ofs.close();
 }
